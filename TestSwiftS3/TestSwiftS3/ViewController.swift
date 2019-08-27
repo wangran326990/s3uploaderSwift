5cb4cae7d8705ec65ac4707fb903ec7598368b35
Ran Wang <ran.wang@yuja.com>
2019-08-27 14:33:03 -0400

    First Commit

---
+++ TestSwiftS3/TestSwiftS3/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestSwiftS3
+//
+//  Created by YuJa Mac on 2019-08-27.
+//  Copyright © 2019 YuJa Mac. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
